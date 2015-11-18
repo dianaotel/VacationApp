d1794389d11ae3b5926528af5f51654981cc4dbc
Vlad Valean <dr.umarth@gmail.com>
2015-11-18 10:54:47 +0200

    Working on MyRequestPage

--- VacationApp/src/test/java/pages/MyRequestsPage.java
+++ VacationApp/src/test/java/pages/MyRequestsPage.java
@@ -4,6 +4,11 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
+import static ch.lambdaj.Lambda.convert;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import ch.lambdaj.function.convert.Converter;
 import helpers.Constants;
 
 @DefaultUrl(Constants.MY_REQUESTS_PAGE_URL)
@@ -135,3 +140,4 @@ public class MyRequestsPage extends PageObject {
 		itemsPerpage.click();
 	}
 }
+
